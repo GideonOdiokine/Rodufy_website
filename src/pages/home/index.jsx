1cf8e5aea8c42804cda2a33d1b6f6d551c94d935
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-04-06 11:39:52 +0100

    added styling for homepage

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import MiniHeader from "./MiniHeader";
+import logo from "../../assets/image/logo.png";
 import "./styles/home.scss";
 
 const index = () => {
   return (
     <div className="home">
       <MiniHeader />
-      <section className="hero"></section>
+      <section className="hero">
+        <div className="hero__container">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
